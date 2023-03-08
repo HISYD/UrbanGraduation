98b2bdb9d7735185a7ba134d1e87e82f2bb98ad5
HISYD <HISYD-P@outlook.com>
2023-03-09 01:31:57 +0800

    UPDATE

--- BaiduParser/MyShp.py
+++ BaiduParser/MyShp.py
@@ -2,31 +2,20 @@ import shapefile
 import json
 
 def read_shp(path):
-    json_path = 'GeoJson.json'
+    # json_path = 'GeoJson.json'
+    # print(path)
+    # reader = shapefile.Reader(path, encoding='gbk') #你他妈的
+    # record = reader.record(0)
+    # shape_record = reader.shapeRecord(0)
+    # print(reader.shape(0).shapeType)
+    #
+    # shapefile.POLYGON
+    # print(record)
+    # print(shape_record.__geo_interface__)
 
-    reader = shapefile.Reader(path, encoding='gbk') #你他妈的
+    path = 'D:/Architecture/UrbanGraduation/TestData/上海市/上海市_市区一级道路.shp'
+    print(path)
+    reader = shapefile.Reader(path, encoding='gbk')  # 你他妈的
     record = reader.record(0)
     shape_record = reader.shapeRecord(0)
     print(reader.shape(0).shapeType)
-
-    shapefile.POLYGON
-    print(record)
-    print(shape_record.__geo_interface__)
-    # print(reader.fields)
-
-    # print(len(records))
-    # print(len(fields))
-    # json.dump()
-
-    # with open(json_path, mode='w') as fp:
-    #     fp.write()
-    # reader = shapefile.Reader(path)
-    # for i in range(len(reader)):
-    #     cur_shape = reader.shape(i)
-    #
-    #     if(len(cur_shape.parts) != 1):
-    #         print(cur_shape)
-    #         print(cur_shape.parts)
-    #     # print(cur_shape)
-        # print(cur_shape.parts)
-
--- BaiduParser/main.py
+++ BaiduParser/main.py
@@ -2,7 +2,7 @@ import MyShp
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    file_path = 'GIS\\西湖区_行人道路.shp'
+    file_path = 'GIS\\上海市_市区一级道路.shp'
 
     MyShp.read_shp(file_path)
     print('---\nEND\n---')
